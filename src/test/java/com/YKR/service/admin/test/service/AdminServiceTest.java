8d1a10c59280db7cc18b53ad2e84e5b453ac914e
cser <1245053895@qq.com>
2019-10-15 21:44:58 +0800

    dev

---
+++ src/test/java/com/YKR/service/admin/test/service/AdminServiceTest.java
@@ -0,0 +1,20 @@
+package com.YKR.service.admin.test.service;
+
+import com.YKR.service.admin.ServiceAdminApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServiceAdminApplication.class)
+public class AdminServiceTest {
+    @Test
+    public void regisetr(){
+
+    }
+    @Test
+    public void login (){
+
+    }
+}
